d1094b9600d8ed333677b26645286938fab95aa5
vicki <surajbarik2003@gmail.com>
2025-01-18 19:25:05 +0530

    updated

--- app/src/main/java/com/example/bookshelfapp/model/AppContainer.kt
+++ app/src/main/java/com/example/bookshelfapp/model/AppContainer.kt
@@ -1,33 +1,33 @@
 package com.example.bookshelfapp.model
 
-import com.example.bookshelfapp.network.ApiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 
-interface AppContainer {
-    val repository: Repository
-}
-
-class DefaultAppContainer: AppContainer {
-
-    private val baseurl = "https://www.googleapis.com/books/v1/"
-
+object RetrofitBuilder {
     private val json = Json {
         ignoreUnknownKeys = true
+        coerceInputValues = true
     }
 
-    private val retrofit = Retrofit.Builder()
-        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseurl)
+    private val client = OkHttpClient.Builder()
+        .addInterceptor { chain ->
+            val request = chain.request()
+            val response = chain.proceed(request)
+            response
+        }
         .build()
 
-    private val retrofitService: ApiService by lazy {
-        retrofit.create(ApiService :: class.java)
-    }
-
-    override val repository: Repository by lazy {
-        NetworkRepository(retrofitService)
+    fun <T> buildService(serviceClass: Class<T>, baseUrl: String): T {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(client)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+            .build()
+            .create(serviceClass)
     }
 }
