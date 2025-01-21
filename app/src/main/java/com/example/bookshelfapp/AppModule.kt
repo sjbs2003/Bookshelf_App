0098be4ba04a5cccc928cf1d8037639f5d4cc2f1
vicki <surajbarik2003@gmail.com>
2025-01-21 19:46:29 +0530

    implemented

---
+++ app/src/main/java/com/example/bookshelfapp/AppModule.kt
@@ -0,0 +1,20 @@
+package com.example.bookshelfapp
+
+import com.example.bookshelfapp.model.NetworkRepository
+import com.example.bookshelfapp.model.Repository
+import com.example.bookshelfapp.network.ApiServiceFactory
+import com.example.bookshelfapp.viewModel.DetailViewModel
+import com.example.bookshelfapp.viewModel.SearchViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val networkModule = module {
+    single { ApiServiceFactory.createGoogleBooksService() }
+    single { ApiServiceFactory.createOpenLibraryService() }
+    single<Repository> { NetworkRepository(get(), get()) }
+}
+
+val viewModelModule = module {
+    viewModel { SearchViewModel(get()) }
+    viewModel { (volumeId: String) -> DetailViewModel(get(), volumeId) }
+}
